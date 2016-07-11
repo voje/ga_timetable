177fdf1dc803a54252d7d72d2c821b24366ab36a
voje <kristjan.voje@gmail.com>
2016-07-11 21:48:16 +0200

    working on eval_function

--- functions.R
+++ functions.R
@@ -33,6 +33,48 @@ create_mapping_matrix <- function(participants, activities, data) {
       }
     }
   }
-  
   return(m);
 }
+
+helper_create_chromosome <- function(mapping_matrix) {
+  chr <- numeric();
+  for (i in 1:nrow(mapping_matrix)) {
+    quadruple <- which(mapping_matrix[i,] == 1);
+    quadruple <- sample(quadruple);
+    for (j in 1:length(quadruple)) {
+      chr <- c(chr, decimal2binary(quadruple[j], 4));
+    }
+  }
+  return(chr);
+}
+
+my_init_pop <- function(object) {
+  if (!exists("mapping_matrix")) {
+    return(FALSE);
+  }
+  n_iter <- object@popSize;
+  m <- numeric();
+  for (i in 1:n_iter) {
+    m <- rbind(m, helper_create_chromosome(mapping_matrix));
+  }
+  return(m);
+}
+
+get_nBits <- function(mapping_matrix) {
+  n <- nrow(mapping_matrix);
+  return(n*4*4);
+}
+
+eval_function <- function(x) {
+  #this is where things get fun
+  
+}
+
+
+
+
+
+
+
+
+
--- main.R
+++ main.R
@@ -1,7 +1,9 @@
+library(GA);
 source("functions.R");
 
 data <- read.table("data/delavnice.csv", sep=",", header=T);
 activities <- create_activities_vector(data);
 participants <- create_participants_vector(data);
-mm <- create_mapping_matrix(participants, activities, data);
+mapping_matrix <- create_mapping_matrix(participants, activities, data);
+
 
