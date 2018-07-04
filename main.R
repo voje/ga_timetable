1cbdbbb0799b916da2898d81404c2ee47512868e
voje <kristjan.voje@gmail.com>
2018-07-04 13:18:18 +0200

    old R code

--- main.R
+++ main.R
@@ -12,10 +12,13 @@ age_weight = 1;
 #num_weight - how much do evenly assigned groups matter
 num_weight = 0.5;
 #
-n_iter = 200;
+n_iter = 50;
 p_cross = 0.7;
 p_mut = 0.7;
 selected_monitor = plot; #gaMonitor, plot
 selected_selection=ga_nlrSelection #seems best so far
 
+#Run GA
 GA <- ga(type="binary", fitness=my_fitness_function1, nBits=get_nBits(mapping_matrix), population=my_init_pop, selection=selected_selection, crossover=my_crossover, mutation=my_mutation, pcrossover=p_cross, pmutation=p_mut, maxiter=n_iter, monitor=selected_monitor);
+
+#Test results
